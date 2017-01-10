ce53a2bd0ca01cc75f609050bdd887e4593a4453
Maetz007 <maetz007@yahoo.com>
2017-01-10 16:09:48 -0600

    initial

--- server/app.js
+++ server/app.js
@@ -3,31 +3,31 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var session = require('express-session');
-var passport = require('./strategies/user-mongo.js');
+// var session = require('express-session');
+// var passport = require('./strategies/user-mongo.js');
 
 app.listen(process.env.PORT || 9003, function(){ console.log("URL Successful"); });
 
-app.use(express.static('server/public'));
+app.use(express.static('public'));
 
-var mongobUri = 'mongob://patience1097:poopoo12@ds161505.mlab.com:61505/patience-chat';
-mongoose.connect(mondodbUri);
+// var mongodbUri = 'mongodb://dnd_tools:poopoo12@ds159328.mlab.com:59328/dnd_tools';
+// mongoose.connect(mongodbUri);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(session({
-   secret: 'secret',
-   key: 'user',
-   resave: 'true',
-   saveUninitialized: false,
-   cookie: {maxage: 60000, secure: false}
-})); // end session
+// app.use(session({
+//    secret: 'secret',
+//    key: 'user',
+//    resave: 'true',
+//    saveUninitialized: false,
+//    cookie: {maxage: 60000, secure: false}
+// })); // end session
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/');
+// app.use('/');
 
 app.get('/*', function(req,res){
   console.log('You Are in L');
